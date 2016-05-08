e8c611eb959ac1ffafbc31da93db52cbb69343c6
addshore <addshorewiki@gmail.com>
2016-05-08 13:35:17 +0100

    run composer script on all skins / exts

--- src/commands/composer.js
+++ src/commands/composer.js
@@ -7,29 +7,57 @@ var
 
 var error = clc.red.bold;
 
+var updateRequirement = function (type, name, packageName, version, dryRun) {
+  var jsonPath = path.resolve( MediaWikiDirectory.getAnnexPath(type, name) + '/composer.json' );
+  if( ComposerJson.updateRequirement(jsonPath, packageName, version, dryRun) ){
+    console.log(type + ' ' + name + ' requirements ' + packageName + ' : ' + version);
+  }
+};
+
 exports.run = function (argv) {
   // Bail if stuff is missing
-  if((!argv.skin && !argv.extension) || argv.skin === true || argv.extension === true) {
-    console.log(error("You must specify an skin or extension to update."));
+  if (
+    (!argv.skin && !argv.extension && !argv.skins && !argv.extensions) ||
+    argv.skin === true ||
+    argv.extension === true
+  ) {
+    console.log(error("You must specify something to update."));
     return;
   }
-  if( !argv.package || argv.package === true ) {
+  if (!argv.package || argv.package === true) {
     console.log(error("You must specify a package to update."));
     return;
   }
-  if( !argv.version || argv.version === true ) {
+  if (!argv.version || argv.version === true) {
     console.log(error("You must specify a version to update to."));
     return;
   }
-
-  var dir = null;
-  if(argv.skin) {
-    dir = MediaWikiDirectory.getAnnexPath('skin',argv.skin);
-  }
-  if(argv.extension) {
-    dir = MediaWikiDirectory.getAnnexPath('extension',argv.extension);
+  if(argv.dry) {
+    console.log('Doing dry run!');
   }
 
-  ComposerJson.updateRequirement( path.resolve(dir + '/composer.json'), argv.package, argv.version );
+  var skins = [];
+  var extensions = [];
+  if(argv.skins) {
+    skins = skins.concat( MediaWikiDirectory.getAnnexes('skin') );
+  }
+  if(argv.extensions) {
+    extensions = extensions.concat( MediaWikiDirectory.getAnnexes('extension') );
+  }
+  if(!argv.skins && !argv.extensions) {
+    if (argv.skin) {
+      skins.push( argv.skin );
+    }
+    if (argv.extension) {
+      extensions.push( argv.extension );
+    }
+  }
+
+  for (var i = 0; i < skins.length; i++) {
+    updateRequirement('skin', skins[i], argv.package, argv.version, argv.dry);
+  }
+  for (var j = 0; j < extensions.length; j++) {
+    updateRequirement('extension', extensions[j], argv.package, argv.version, argv.dry);
+  }
 
 };
--- src/modules/ComposerJson.js
+++ src/modules/ComposerJson.js
@@ -1,35 +1,33 @@
 var
-  fs = require('fs-extra'),
-  MediaWikiDirectory = require('./../modules/MediaWikiDirectory'),
-  path = require('path'),
-  clc = require('cli-color');
+  fs = require('fs-extra');
 
-var error = clc.red.bold;
-
-exports.updateRequirement = function( jsonPath, packageName, newVersion ) {
+exports.updateRequirement = function( jsonPath, packageName, newVersion, dryRun ) {
   try {
     fs.accessSync(jsonPath, fs.F_OK);
   }
   catch(err) {
-    console.log(error('composer.json not found'));
     return false
   }
+  var changes = false;
   var composerJson = JSON.parse(fs.readFileSync(jsonPath, 'utf8'));
   if(
     composerJson.hasOwnProperty('require') &&
     composerJson['require'].hasOwnProperty(packageName) &&
     composerJson['require'][packageName] != newVersion
   ) {
-    console.log(jsonPath + ' require ' + packageName + ':' + newVersion);
     composerJson['require'][packageName] = newVersion;
+    changes = true;
   }
   if(
     composerJson.hasOwnProperty('require-dev') &&
     composerJson['require-dev'].hasOwnProperty(packageName) &&
     composerJson['require-dev'][packageName] != newVersion
   ) {
-    console.log(jsonPath + ' require-dev ' + packageName + ':' + newVersion);
     composerJson['require-dev'][packageName] = newVersion;
+    changes = true;
   }
-  fs.writeFileSync(jsonPath,JSON.stringify(composerJson, null, '\t'),'utf8')
+  if(changes && !dryRun) {
+    fs.writeFileSync(jsonPath,JSON.stringify(composerJson, null, '\t'),'utf8')
+  }
+  return changes;
 };
