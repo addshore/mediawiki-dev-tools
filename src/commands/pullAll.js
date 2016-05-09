5b518577d927525607557e0ec67552818f3e14ff
addshore <addshorewiki@gmail.com>
2016-05-09 17:25:58 +0100

    Async pull all

--- src/commands/pullAll.js
+++ src/commands/pullAll.js
@@ -3,32 +3,51 @@ require('shelljs/global');
 var
   fs = require('fs-extra'),
   path = require('path'),
+  async = require('async'),
   process = require('process'),
   MediaWikiDirectory = require('./../modules/MediaWikiDirectory'),
   clc = require('cli-color');
 
 var error = clc.red.bold;
 
-var pullDirectory = function(dir,useRemote) {
+var pullDirectory = function(task, callback) {
+  var dir = task.dir;
+  var useRemote = task.useRemote;
   cd(dir);
   console.log('Queuing ' + dir);
-  exec('git stash save "mwdev --pull-all"',{silent:true});
-  exec('git checkout master',{silent:true});
-  if(useRemote) {
-    var oldRemote = exec('git config --get remote.origin.url',{silent:true}).stdout.trim();
-    exec('git remote set-url origin ' + useRemote,{silent:true});
-  }
-  exec('git reset --hard origin/master',{silent:true});
-  exec('git pull &',{silent:true});
-  if(useRemote) {
-    exec('git remote set-url origin ' + oldRemote,{silent:true});
-  }
-  console.log('Done ' + dir);
+  exec('git stash save "mwdev --pull-all"',{silent:true,async:true}, function() {
+    exec('git checkout master',{silent:true});
+    if(useRemote) {
+      var oldRemote = exec('git config --get remote.origin.url',{silent:true}).stdout.trim();
+      exec('git remote set-url origin ' + useRemote,{silent:true});
+    }
+    exec('git reset --hard origin/master',{silent:true});
+    exec('git pull &',{silent:true,async:true}, function() {
+      if(useRemote) {
+        exec('git remote set-url origin ' + oldRemote,{silent:true});
+      }
+      console.log('Done ' + dir);
+      callback();
+    });
+  });
 };
 
 exports.run = function( argv ) {
   var root = MediaWikiDirectory.getPath();
   var i;
+
+  var asyncLevel = 1;
+  if(argv.async) {
+    asyncLevel = 25;
+    if(argv.async !== true) {
+      asyncLevel = argv.async;
+    }
+  }
+  var q = async.queue(pullDirectory, asyncLevel);
+
+  q.drain = function() {
+    console.log('All items have been processed');
+  };
 
   //TODO factor this out to somewhere...
   // Get the user.name registered in git and bail if there isn't one
@@ -39,23 +58,23 @@ exports.run = function( argv ) {
   }
 
   if(argv.all){
-    pullDirectory(root);
+    q.push({dir:root}, function(err){})
   }
   if(argv.skins||argv.all) {
     var skins = MediaWikiDirectory.getAnnexes('skin');
     for (i = 0; i < skins.length; i++) {
-      pullDirectory(
-        path.resolve( root + '/skins/' + skins[i] ),
-        'ssh://' + gitUser + '@gerrit.wikimedia.org:29418/mediawiki/skins/' + skins[i]
+      q.push(
+        {dir:path.resolve( root + '/skins/' + skins[i] ),useRemote:'ssh://' + gitUser + '@gerrit.wikimedia.org:29418/mediawiki/skins/' + skins[i]},
+        function(err){}
       );
     }
   }
   if(argv.extensions||argv.all) {
     var annexes = MediaWikiDirectory.getAnnexes('extension');
     for (i = 0; i < annexes.length; i++) {
-      pullDirectory(
-        path.resolve( root + '/extensions/' + annexes[i] ),
-        'ssh://' + gitUser + '@gerrit.wikimedia.org:29418/mediawiki/extensions/' + annexes[i]
+      q.push(
+        {dir:path.resolve( root + '/extensions/' + annexes[i] ),useRemote:'ssh://' + gitUser + '@gerrit.wikimedia.org:29418/mediawiki/extensions/' + annexes[i]},
+        function(err){}
       );
     }
   }
